2141e660f2bbb14920d02ebbe2506b418a6c4950
Aditya10403 <adityashukla@gmail.com>
2024-05-23 12:01:00 +0530

    added sentry auth token

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ import { FloatingNav } from "@/components/ui/FloatingNavbar";
 
 const Home = () => {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5 font-normaltext">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-clip mx-auto sm:px-10 px-5 font-normaltext">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
         <Hero />
